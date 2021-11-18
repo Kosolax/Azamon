1024b4487292e07b5e312a591dcb7b4b6d31e268
Mininger Mael <ma.mininger@gmail.com>
2021-11-18 09:06:01 +0100

    hehe boy

--- Assets/Scripts/EndingPoint.cs
+++ Assets/Scripts/EndingPoint.cs
@@ -9,7 +9,7 @@ public class EndingPoint : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (this.GameManager.CurrentMission == this.Mission.MissionNumber)
+        if (this.GameManager.CurrentMission == this.Mission.MissionNumber && this.Mission.IsStarted)
         {
             Player player = other.GetComponent<Player>();
             if (player != null && player.HasPackage)
